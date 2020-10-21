a3b9d34ad4c23bf350a2d4eac257a461fb9a72f1
ZhengYu <zhengy@gato.cn>
2020-10-21 10:32:41 +0800

    彩色打印

---
+++ color/printColourText.go
@@ -0,0 +1,8 @@
+package color
+
+import "fmt"
+
+// Foreground color, background color, action 具体值参阅readme
+func BuildColorText(text string, fg, bg, act int8) string {
+	return fmt.Sprintf("%c[%d;%d;%dm %s %c[0m", 0x1b, act, bg, fg, text, 0x1b)
+}
